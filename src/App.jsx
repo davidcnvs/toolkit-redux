2ed4aa3a50f42647067d08431d666943df532a79
David <david.cnvs@gmail.com>
2022-07-05 02:51:32 -0400

    snippet slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import logo from './logo.svg'
 import './App.css'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementBy } from './store/slices/counter';
 
 function App() {
   
   const {counter} = useSelector(state => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>count is: {counter}</p>
         <p>
-          <button type="button" onClick={() => {}}>
-            count is: {counter}
+          <button type="button" onClick={() => dispatch(increment()) }>
+            Increment
+          </button>
+          <button type="button" onClick={() => dispatch(decrement()) }>
+            Decrement
+          </button>
+          <button type="button" onClick={() => dispatch(incrementBy(2)) }>
+            Increment by 2
           </button>
         </p>
         

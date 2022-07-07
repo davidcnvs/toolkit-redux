a77a63e511d6713780bf0f4eebe297979b5f4f40
David <david.cnvs@gmail.com>
2022-07-07 16:01:16 -0400

    seccion lista

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,31 +1,41 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { pokemonSlice } from "./store/slices/pokemon/pokemonSlice";
 import { getPokemons } from "./store/slices/pokemon/thunks";
 
 
 export const PokemonApp = () => {
 
+
+  const {isLoading, pokemons, page} = useSelector(state => state.pokemons);
   const dispatch = useDispatch();
 
 
   useEffect(() => {
     dispatch(getPokemons());
-  
   }, [])
   
-
-
 
   return (
     <>
         <h1>PokemonApp</h1>
         <hr />
         
+        <span>Loading: {isLoading ? 'True' : 'False'}</span>
+
         <ul>
-          <li>Hola</li>
-          <li>Hola</li>
-          <li>Hola</li>
+          {
+          pokemons.map( ({name}) => (
+            <li key={name}>{name}</li>
+          ))
+          }
         </ul>
+
+        <button
+        disabled={isLoading}
+        onClick={ ()=>{dispatch(getPokemons(page))} }>
+          Next
+        </button>
     </>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { todosApi } from './apis';
 import { counterSlice } from './slices/counter';
 import { pokemonSlice } from './slices/pokemon/pokemonSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
-    pokemons: pokemonSlice.reducer
+    pokemons: pokemonSlice.reducer,
+
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todosApi.middleware)
 })
